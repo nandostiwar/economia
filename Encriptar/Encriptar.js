22abe13d2a7744c3a05bee310dc2252663e8ea66
Oswal13 <Villalvaoswaldo@gmail.com>
2023-08-26 18:03:54 -0500

    Encriptar

---
+++ Encriptar/Encriptar.js
@@ -0,0 +1,30 @@
+const botonJS = document.getElementById("botonJS");
+const inputJS = document.getElementById("entradaJS");
+const outputJS = document.getElementById("salidaJS");
+
+botonJS.addEventListener("click", function() {
+    const textoEntrada = inputJS.value;
+    const textoConvertido = encriptarTexto(textoEntrada);
+    
+    outputJS.textContent = textoConvertido;
+})
+
+function encriptarTexto(texto) {
+    const valores = {
+        'a': 'x',
+        'e': 'y',
+        'i': 'z',
+        'o': 'w',
+        'u': 'k',
+        ' ': ''
+    };
+
+    let result = '';
+
+    for (let i = 0; i < texto.length; i++) {
+        const caracter = texto[i].toLowerCase();
+        result += valores[caracter] || texto[i];
+    }
+
+    return result;
+}
