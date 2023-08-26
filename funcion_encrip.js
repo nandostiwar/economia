deaf94fc932c3ad256fd3d5220c638e1803ca500
Danny25 <142683278+Dannyguz25@users.noreply.github.com>
2023-08-26 17:14:23 -0500

    mi programa encriptado

---
+++ funcion_encrip.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botonJS = document.getElementById("botonJS");
+    const entradaJS = document.getElementById("entradaJS");
+    const salidaJS = document.getElementById("salidaJS");
+
+    const sustituciones = {
+        'a': 'x',
+        'e': 'y',
+        'i': 'z',
+        'o': 'w',
+        'u': 'k',
+        ' ': 'cero'
+    };
+
+    botonJS.addEventListener("click", function () {
+        const textoEntrada = entradaJS.value;
+        const textoEncriptado = encriptarTexto(textoEntrada);
+        salidaJS.textContent = textoEncriptado;
+    });
+
+    function encriptarTexto(texto) {
+        let textoEncriptado = "";
+        for (let i = 0; i < texto.length; i++) {
+            const caracter = texto[i].toLowerCase();
+            if (sustituciones[caracter] !== undefined) {
+                textoEncriptado += sustituciones[caracter];
+            } else {
+                textoEncriptado += caracter;
+            }
+        }
+        return textoEncriptado;
+    }
+});
+
+
+
