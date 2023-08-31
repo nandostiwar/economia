a46ed5c1db698d1ce41e34ecfd44cfd73340a290
Juan Sebastian Gutierrez <juan.gutierrez02@unicatolica.edu.co>
2023-08-30 23:52:00 -0500

    Finalización del juego ahorcado js

--- ahorcado_juanse93/juego.js
+++ ahorcado_juanse93/juego.js
@@ -1,21 +1,30 @@
 
 const contPalabra = document.getElementById("contenedorPalabra")
 const contLetra = document.getElementById("contenedorLetra")
+const contReinicio = document.getElementById("contenedorReinicio")
 
 const palabra = document.getElementById("palabra")
-const letra = document.getElementById("letra")
+const letra_ = document.getElementById("letra")
 const vidas = document.getElementById("vidas")
 const btnEmpezar = document.getElementById("btnEmpezar")
 const btnJugar = document.getElementById("btnJugar")
+const btnReinicio = document.getElementById("btnReinicio")
 const salida = document.getElementById("salida")
 
 btnEmpezar.addEventListener('click', empezar)
 btnJugar.addEventListener('click', suerte)
+btnReinicio.addEventListener('click', reinicio)
 
+//Función replaceAt para reemplazar un caracter en una posición //
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+let palabraGuiones
 function empezar() {
-    let palabraGuiones = palabra.value.replace(/./g, "_ ")
+    palabraGuiones = palabra.value.replace(/./g, "_ ")
     salida.innerHTML = palabraGuiones
-    alert(palabra.value + " " + palabraGuiones)
+    console.log(palabra.value + " " + palabraGuiones)
     contPalabra.style.display = "none"
     contLetra.style.display = "block"
 }
@@ -23,21 +32,44 @@ function empezar() {
 let ttlVidas = 5
 function suerte() {
 
-    if (letra.value.trim() === "") {
+    const letra = document.querySelector('#letra').value
+
+    if (letra.trim() === "") {
         alert("Debes digitar una letra para seguir")
     } else {
-        let flag = 0
+        let letrasEncontradas = 0
         for (let i = 0; i < palabra.value.length; i++) {
-            if (letra.value === palabra.value[i]) {
-                flag++
-                alert("exito: encontraste la letra: "+letra.value)
+            if (letra === palabra.value[i]) {
+                letrasEncontradas++
+                console.log("exito: encontraste la letra: " + letra)
+                console.log("letrasEncontradas: " + letrasEncontradas)
+                console.log(palabraGuiones)
+                palabraGuiones = palabraGuiones.replaceAt(i*2, letra)
+                console.log(palabraGuiones)
+                salida.innerHTML = palabraGuiones
+                letra_.value = ''
             }
         }
-            if(flag == 0){
-                ttlVidas--
-                alert("Perdiste | Vidas: " + ttlVidas)
-                vidas.innerHTML = ttlVidas
-            }
+        if (letrasEncontradas == 0) {
+            ttlVidas--
+            alert("Perdiste | Vidas: " + ttlVidas)
+            vidas.innerHTML = ttlVidas
+            letra_.value = ''
+        }
+        if (ttlVidas == 0) {
+            gameOver()
+        }
 
     }
+    //salida.innerHTML = palabraGuiones
+}
+
+function gameOver(){
+    contLetra.style.display = "none"
+    contReinicio.style.display = "block"
+    palabra.value = ''
+}
+function reinicio(){
+    contReinicio.style.display = "none"
+    contPalabra.style.display = "block"
 }
