a22fdba0f6edb3f8d950f00a08a9655c7118406e
Jose-Hernando Mosquera-DeLaCruz, Ph.D.(c), M.Sc., Eng <45375029+nandostiwar@users.noreply.github.com>
2023-09-21 15:44:22 -0500

    parcial1

---
+++ parcial1/src/components/Message.jsx
@@ -0,0 +1,9 @@
+function Message({message}){
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+export default Message;
---
+++ parcial1/src/components/Orden.jsx
@@ -0,0 +1,26 @@
+import '../styles/Orden.css';
+import { useState } from 'react';
+
+function Orden({turno, pedido, mesa, mesero}){
+    const [pendiente, setPendiente] = useState(true);
+
+    function validate(e){
+        if(pendiente===true){
+            setPendiente(false);
+        }else if(pendiente===false){
+            setPendiente(true);
+        }
+    }
+    return (
+        <div className="orden">
+            <h2>Turno {turno}</h2>
+            <h2>Pedido {pedido}</h2>
+            <h2>Mesa {mesa}</h2>
+            <h2>Mesero {mesero}</h2>
+
+            {pendiente ? (<button onClick={validate}>PENDIENTE</button>):(<button onClick={validate}>ENTREGADO</button>)}
+        </div>
+    )
+}
+
+export default Orden;
