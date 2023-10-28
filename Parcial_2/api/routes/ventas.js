447b03ed304096267795c578d8b5a629f34b972a
Danny25 <142683278+Dannyguz25@users.noreply.github.com>
2023-10-28 15:18:26 -0500

    Delete Parcial_2 directory

--- Parcial_2/api/routes/ventas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const ventasController = require('../controllers/VentasController');
-
-const router = express.Router();
-
-router.get('/mostrar', ventasController.getVentas);
-router.post('/crear', ventasController.crearVentas);
-
-module.exports = router;
